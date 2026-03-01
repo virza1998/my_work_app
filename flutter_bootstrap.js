7262ea2adeb8d50d3973f4de0da6c0f6ec8746b3
virza1998 <virza1998@gmail.com>
2026-03-01 05:56:37 +0000

    Deploying to gh-pages from @ virza1998/blanks_001@b68bfb242fae59a584e5432f3ea2c4aab3ad1db3 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "490951453" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1149393962" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
